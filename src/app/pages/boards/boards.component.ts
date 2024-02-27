c00f7113ad854bfbb61a280c9b89cb2c671c3fd6
IsraGar <isragarciahdez@gmail.com>
2024-02-27 11:39:32 -0600

    Se agrega acordeon de Angular CDK

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBox, faWaveSquare, faClock, faC } from '@fortawesome/free-solid-svg-icons';
+import { faBox, faWaveSquare, faClock, faAngleDown, faAngleUp, faHeart, faBorderAll, faUsers, faGear } from '@fortawesome/free-solid-svg-icons';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule, CommonModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -16,5 +18,47 @@ export class BoardsComponent {
   faBox = faBox;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faAngleDown = faAngleDown;
+  faAngleUp = faAngleUp;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
+
+  items = [
+    {
+      label: 'Item 1',
+      items: [
+        {
+          label: 'Sub item 1.1'
+        },
+        {
+          label: 'Sub item 1.2'
+        }
+      ]
+    },
+    {
+      label: 'Item 2',
+      items: [
+        {
+          label: 'Sub item 2.1'
+        }
+      ]
+    },
+    {
+      label: 'Item 3',
+      items: [
+        {
+          label: 'Sub item 3.1'
+        },
+        {
+          label: 'Sub item 3.2'
+        },
+        {
+          label: 'Sub item 3.3'
+        }
+      ]
+    }
+  ]
 
 }
