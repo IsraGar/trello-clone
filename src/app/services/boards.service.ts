2e5b5ef35eb8f21da5d3ab258c96f1afed858f7f
IsraGar <isragarciahdez@gmail.com>
2024-03-04 16:23:16 -0600

    Calculo de posicion de las cards

--- src/app/services/boards.service.ts
+++ src/app/services/boards.service.ts
@@ -3,6 +3,7 @@ import { Injectable, inject } from '@angular/core';
 import { environment } from '../environments/environment.prod';
 import { checkToken } from '../interceptors/token.interceptor';
 import { Board } from '../models/board.model';
+import { Card } from '../models/cards.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,33 @@ import { Board } from '../models/board.model';
 export class BoardsService {
   private http = inject(HttpClient);
   apiUrl = environment.API_URL;
+  bufferSpace = 65535;
 
   constructor() { }
 
   getBoard(id: Board['id']){
     return this.http.get<Board>(`${this.apiUrl}/api/v1/boards/${id}`, { context: checkToken() })
+  }
+
+  getPosition(cards: Card[], currentIndex: number){
+    if(cards.length === 1){
+      return this.bufferSpace;
+    }
+    if(cards.length > 1 && currentIndex === 0){
+      const onTopPosition = cards[1].position;
+      return onTopPosition / 2;
+    }
+    const lastIndex = cards.length - 1;
+    if(cards.length > 2 && currentIndex > 0 && currentIndex < lastIndex){
+      const prevPosition = cards[currentIndex - 1].position;
+      const nextPosition = cards[currentIndex + 1].position;
+      return (prevPosition + nextPosition) / 2;
+    }
+    if(cards.length > 1 && currentIndex === lastIndex){
+      const onBottomPosition = cards[lastIndex - 1].position;
+      return onBottomPosition + this.bufferSpace;
+    }
+    return 0;
   }
 
 }
